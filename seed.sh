45f463c874ffa37286a2ed696823b11675b3f465
Stephen Leach <sfkleach@gmail.com>
2014-03-27 19:04:11 +0000

    Don't generate images as part of the seed process,

--- seed.sh
+++ seed.sh
@@ -2,11 +2,9 @@
 # Usage: ./seed.sh JSON.json DATABASE.db IMAGE.png
 JSON=$1
 DB=$2
-IMG=$3
 if [ -e ${DB} ]
 then
 	mv ${DB} ${DB}-
 fi
 sqlite3 ${DB} < seed.sql
 python3 seed.py ${DB} ${JSON}
-python3 image.py ${DB} ${IMG}
