c7ccbf4991a860bc34c23dbc5fd30a7fd0d83b62
Stephen Leach <sfkleach@gmail.com>
2014-03-27 19:03:46 +0000

    Pass through all the parameters.

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-DB=$1
 CLEAR="delete from team; delete from hex; delete from world; delete from faction; vacuum;"
 if [ -e before.db ]
 then
@@ -17,6 +16,6 @@ else
 	echo 'Creating after.db'
 	sqlite3 after.db < seed.sql
 fi
-./simulation --db=${DB}
+./simulation $*
 python3 image.py before.db before.png
 python3 image.py after.db after.png
